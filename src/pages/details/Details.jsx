74196cec824168997adeae047c68736b7d9897bb
nikita-rawat <nikitarwt789@gmail.com>
2023-04-23 13:04:42 +0530

    changes

--- src/pages/details/Details.jsx
+++ src/pages/details/Details.jsx
@@ -1,6 +1,5 @@
 import React from 'react'
 import { useParams } from 'react-router-dom'
-import "./style.scss"
 import DetailsBanner from './detailsBanner/DetailsBanner'
 import useFetch from "../../hooks/useFetch"
 import Cast from './cast/Cast'
