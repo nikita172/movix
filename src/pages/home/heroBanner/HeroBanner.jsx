d7aa74cef946811f79c97aec8eeffc7df2989c3c
nikita-rawat <nikitarwt789@gmail.com>
2023-04-23 12:27:54 +0530

    movix app completed

---
+++ src/pages/home/heroBanner/HeroBanner.jsx
@@ -0,0 +1,54 @@
+import React, { useState, useEffect } from 'react'
+import { useNavigate } from 'react-router-dom';
+import { useSelector } from 'react-redux';
+import "./style.scss"
+import Img from '../../../components/lazyLoadImage/Img';
+import ContentWrapper from '../../../components/contentWrapper/ContentWrapper';
+import useFetch from '../../../hooks/useFetch';
+
+const HeroBanner = () => {
+  const [background, setBackground] = useState("");
+  const [query, setQuery] = useState("");
+  const navigate = useNavigate();
+  const { url } = useSelector(state => state.home);
+  const { data, loading } = useFetch("/movie/upcoming");
+
+  useEffect(() => {
+    const bg = url.backdrop + data?.results?.[Math.floor(Math.random() * 20)]?.backdrop_path;
+    setBackground(bg);
+  }, [data]);
+
+  const searchQueryHandler = (e) => {
+    if (e.key === "Enter" && query.length > 0) {
+      navigate(`/search/${query}`);
+    }
+  }
+
+  return (
+    <div className='heroBanner'>
+      {
+        !loading && <div className="backdrop-img">
+          <Img src={background} />
+        </div>
+      }
+      <div className="opacity-layer"></div>
+      <ContentWrapper>
+        <div className="heroBannerContent">
+          <span className="title">Welcome.</span>
+          <span className="subTitle">Millions of movies, TV shows and people to discover. Explore now.</span>
+          <div className="searchInput">
+            <input
+              type='text'
+              placeholder='Search for a movie or TV show....'
+              onKeyUp={searchQueryHandler}
+              onChange={(e) => setQuery(e.target.value)}
+            />
+            <button>Search</button>
+          </div>
+        </div>
+      </ContentWrapper>
+    </div>
+  )
+}
+
+export default HeroBanner
